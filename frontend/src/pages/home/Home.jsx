9166913e8dfbae10c2c9349cc391d2bfedebbfa8
quocthangK64 <thang.nq194839@sis.hust.edu.vn>
2022-08-03 16:21:39 +0700

    show details info of students when choose in the table

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -12,25 +12,11 @@ function Home() {
   <Box sx={{ flexGrow: 1 }} margin={{xs: 1, sm: 3, md: 6 }}>
     <Grid container spacing={{xs: 1, sm:3, md: 6}}>
       <Grid item xs={6} sm={7} md={9}>
-        {/*Left sides Tables*/}
         <Recap/>
         <StudentTable/>
       </Grid>
       <Grid item xs={6} sm={5} md={3}>
-        {/*Right sides Info*/}
-        <StudentInfo studentInfo={
-            {
-                id: 1,
-                firstName:"Hung",
-                lastName:"To",
-                address:"Dinh Cong",
-                dob:"2001-12-22T17:00:00.000Z",
-                highSchool:"Tran Nhan Tong",
-                mathScore:"9.0",
-                literatureScore:"5.0",
-                englishScore:"10.0"
-            }
-            }/>
+        {selectedStudent && <StudentInfo studentInfo={selectedStudent}/>}
       </Grid>
     </Grid>
   </Box>)
