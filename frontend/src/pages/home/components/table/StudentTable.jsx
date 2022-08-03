921b748706e3e382ef026fa3e3380391a7ed8db4
quocthangK64 <thang.nq194839@sis.hust.edu.vn>
2022-08-03 16:43:41 +0700

    change hover row to pointer cursor

--- frontend/src/pages/home/components/table/StudentTable.jsx
+++ frontend/src/pages/home/components/table/StudentTable.jsx
@@ -148,7 +148,7 @@ export default function StudentTable() {
               .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
               .map((row) => {
                 return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.id} onClick={() => {handleTableRowClick(row)}}>
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.id} onClick={() => {handleTableRowClick(row)}} sx={{cursor: 'pointer'}}>
                     {columns.map((column) => {
                       const value = row[column.id];
                       return (
