05ac48fd58228575b9cfdd666581aa6e5eb15250
tranthang <ducthang.280401@gmail.com>
2022-08-08 01:06:12 +0700

    annual commit

--- frontend/src/pages/home/components/newstudentpopup/components/NewStudentInfo.jsx
+++ frontend/src/pages/home/components/newstudentpopup/components/NewStudentInfo.jsx
@@ -1,23 +1,217 @@
-import { Typography } from "@mui/material";
 import * as React from 'react';
-import {Grid, Box} from "@mui/material";
+import {Grid, Box, Typography, Divider, Button} from "@mui/material";
+import TextField from '@mui/material/TextField';
 
-function NewStudentInfo(props) {
+const typoFont = {
+  fontFamily: "san-arif",
+}
+
+const fieldTypoStyle = {
+  ...typoFont,
+  color: "rgb(136, 136, 136)",
+  fontSize: 16,
+  fontWeight: "bold"
+}
+
+const valueTypoStyle = {
+  ...typoFont,
+  color: "black",
+  fontSize: 16,
+  fontWeight: "medium"
+}
+
+const rowStyle = {
+  my: 1
+}
+
+
+function NewStudentInfo() {
+
     const newStudent = {
-        id : number,
-        firstName : string,
-        lastName : string | null,
-        address : string |null,
-        dob : Date,
-        highSchool : string,
-        mathScore : number,
-        literatureScore : number,
-        englishScore : number
+        id : null,
+        firstName : null,
+        lastName : null,
+        address : null,
+        dob: null,
+        highSchool : null,
+        mathScore : null,
+        literatureScore : null,
+        englishScore : null
     }
+    
+    const [id, setId] = React.useState(null);
+    const [firstName, setFirstName] = React.useState(null);
+    const [lastName, setLastName] = React.useState(null);
+    const [dob, setDob] = React.useState(null);
+    const [highSchool, setHighSchool] = React.useState(null);
+    const [mathScore, setMathScore] = React.useState(null);
+    const [literatureScore, setLiteratureScore] = React.useState(null);
+    const [englishScore, setEnglishScore] = React.useState(null);
+
+    const handleOnChange = (event, setFunction) => {
+        setFunction(event.target.value);
+    }
+
+    React.useEffect(() => {
+        setId(null);
+        setFirstName(null);
+        setLastName(null);
+        setDob(null);
+        setHighSchool(null);
+        setMathScore(null);
+        setLiteratureScore(null);
+        setEnglishScore(null);
+    }, []);
+
     return(
         <Box sx={{}}>
            <Grid container sx={{}}>
+                <Grid container item xs={12} sx={{...rowStyle}}>
+                <Grid item xs={6}>
+                    <Typography sx = {{...fieldTypoStyle}}>Student ID</Typography>
+                </Grid>
+                <Grid item xs={6}>                  
+                    <TextField
+                            id="student-id"
+                            placeholder="Student ID*"
+                            helperText=""
+                            value={id}
+                            onChange={(event) => {
+                                handleOnChange(event, setId); 
+                                newStudent.id = event.target.value;
+                            }}
+                        />
+                </Grid>
+                </Grid>
 
+                <Grid container item xs={12} sx={{...rowStyle}}>
+                <Grid item xs={6}>
+                    <Typography sx = {{...fieldTypoStyle}}>First Name</Typography>
+                </Grid>
+                <Grid item xs={6}>                     
+                    <TextField
+                            id="first-name"
+                            placeholder="First Name*"
+                            helperText=""
+                            value={firstName}
+                            onChange={(event) => {
+                                handleOnChange(event, setFirstName); 
+                                newStudent.firstName = event.target.value;
+                            }}
+                        />
+                </Grid>
+                </Grid>
+                                <Grid container item xs={12} sx={{...rowStyle}}>
+                <Grid item xs={6}>
+                    <Typography sx = {{...fieldTypoStyle}}>Last Name</Typography>
+                </Grid>
+                <Grid item xs={6}>                   
+                    <TextField
+                            id="last-name"
+                            placeholder="Last Name*"
+                            helperText=""
+                            value={lastName}
+                            onChange={(event) => {
+                                handleOnChange(event, setLastName); 
+                                newStudent.lastName = event.target.value;
+                            }}
+                        />
+                </Grid>
+                </Grid>
+
+                <Grid container item xs={12} sx={{...rowStyle}}>
+                <Grid item xs={6}>
+                    <Typography sx = {{...fieldTypoStyle}}>DOB</Typography>
+                </Grid>
+                <Grid item xs={6}>                      
+                    <TextField
+                            id="date-of-birth"
+                            placeholder="Date of birth*"
+                            helperText=""
+                            value={dob}
+                            onChange={(event) => {
+                                handleOnChange(event, setDob); 
+                                newStudent.dob = event.target.value;
+                            }}
+                    />
+                </Grid>    
+                </Grid>
+
+                <Grid container item xs={12} sx={{...rowStyle}}>
+                <Grid item xs={6}>
+                    <Typography sx = {{...fieldTypoStyle}}>High School</Typography>
+                </Grid>
+                <Grid item xs={6}>  
+                    <TextField
+                            id="High-School"
+                            placeholder="High School*"
+                            helperText=""
+                            value={highSchool}
+                            onChange={(event) => {
+                                handleOnChange(event, setHighSchool); 
+                                newStudent.highSchool = event.target.value;
+                            }}
+                        />
+                </Grid> 
+                </Grid>
+
+                <Grid container item xs={12} sx={{...rowStyle}}>
+                <Grid item xs={6}>
+                    <Typography sx = {{...fieldTypoStyle}}>Math Score</Typography>
+                </Grid>
+                <Grid item xs={6}>         
+                    <TextField
+                            id="math-score"
+                            placeholder="Math Score*"
+                            helperText=""
+                            value={mathScore}
+                            onChange={(event) => {
+                                handleOnChange(event, setMathScore); 
+                                newStudent.mathScore = event.target.value;
+                            }}
+                        />
+                </Grid>  
+                </Grid>  
+                <Grid container item xs={12} sx={{...rowStyle}}>
+                <Grid item xs={6}>
+                    <Typography sx = {{...fieldTypoStyle}}>Literature Score</Typography>
+                </Grid>
+                <Grid item xs={6}>                   
+                    <TextField
+                            id="literature-score"
+                            placeholder="Literature Score*"
+                            helperText=""
+                            value={literatureScore}
+                            onChange={(event) => {
+                                handleOnChange(event, setLiteratureScore); 
+                                newStudent.literatureScore = event.target.value;
+                            }}
+                        />
+                </Grid>
+                </Grid>
+                <Grid container item xs={12} sx={{...rowStyle}}>
+                <Grid item xs={6}>
+                    <Typography sx = {{...fieldTypoStyle}}>English Score</Typography>
+                </Grid>
+                <Grid item xs={6}>                  
+                    <TextField
+                            id="English-score"
+                            placeholder="English Score*"
+                            helperText=""
+                            value={englishScore}
+                            onChange={(event) => {
+                                handleOnChange(event, setEnglishScore); 
+                                newStudent.englishScore = event.target.value;
+                            }}
+                        />
+                </Grid>
+                </Grid>
+                <Grid container item xs={12}>
+                    <Grid item xs={6}>
+				        <Button sx={{height: '100%', width: '100%'}} variant="contained" startIcon={<NoteAddIcon/>} onClick={props.handleClose}>Cancel</Button>
+				        <Button sx={{height: '100%', width: '100%'}} variant="contained" startIcon={<NoteAddIcon/>} onClick={props.handleClose}>Confirm</Button>
+                    </Grid>
+                </Grid>
             </Grid> 
         </Box>
     );
