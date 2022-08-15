05ae01874fea29bd368b8073465dcbd3743b4929
quocthangK64 <thang.nq194839@sis.hust.edu.vn>
2022-08-16 00:30:20 +0700

    send error or confirm when create or update student

--- Backend/src/student/student.controller.ts
+++ Backend/src/student/student.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, ConsoleLogger, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { Student } from './schema/student.schema';
 import { StudentDTO } from './dto/student.dto';
@@ -13,17 +13,31 @@ export class StudentController {
     }
 
     @Post()
-    createNewStudent(@Body() param : Student) {
-        return this.studentService.createNewStudent(param);
+    async createNewStudent(@Body() param : Student) : Promise<string> {
+        var studentList = this.studentService.findAll();
+        var check = 0;
+        (await studentList).forEach(student => {
+            if(student.id === param.id) {
+                check++;
+            }
+        });
+        if(check === 0) {
+            this.studentService.createNewStudent(param);
+            return "Successfully create student";
+        }else {
+            return "Cannot create student with id = " + param.id;
+        }
     }
 
     @Put()
-    updateStudent(@Body() student : Student) {
-        return this.studentService.updateStudent(student);
+    updateStudent(@Body() student : Student) : string {
+        this.studentService.updateStudent(student);
+        return "Update successfully";
     }
 
     @Delete(':id')
-    deleteStudent(@Param() param) {
-        return this.studentService.deleteStudent(param.id);
+    deleteStudent(@Param() param) : string {
+        return "Delete successfully";
+        this.studentService.deleteStudent(param.id);
     }
 }
--- frontend/src/pages/home/components/studentinformation/StudentInfo.jsx
+++ frontend/src/pages/home/components/studentinformation/StudentInfo.jsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import {Grid, Box, Typography, Button} from "@mui/material";
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
@@ -5,6 +6,8 @@ import {useDispatch, useSelector} from "react-redux";
 import {GradeTable} from "./GradeTable";
 import StudentService from "../../../../api/student/StudentService";
 import {updateRefreshState, updateSelectedStudent} from "../../../../redux/actions";
+import TextField from "@mui/material/TextField";
+import DoneOutlineIcon from '@mui/icons-material/DoneOutline';
 
 const typoFont = {
   fontFamily: "san-arif",
@@ -33,6 +36,20 @@ function StudentInfo(props) {
   const selectedStudent = useSelector((state) => state.selectedStudent)
   const stateRefresh = useSelector((state) => state.stateRefresh);
 
+  const [firstName, setFirstName] = React.useState(selectedStudent.firstName);
+  const [lastName, setLastName] = React.useState(selectedStudent.lastName);
+  const [birth, setDob] = React.useState(selectedStudent.dob);
+  const [address, setAddress] = React.useState(selectedStudent.address);
+  const [highSchool, setHighSchool] = React.useState(selectedStudent.highSchool);
+  React.useEffect(() => {
+    setFirstName(selectedStudent.firstName);
+    setLastName(selectedStudent.lastName);
+    setDob(selectedStudent.dob);
+    setAddress(selectedStudent.address);
+    setHighSchool(selectedStudent.highSchool);
+  }, [selectedStudent])
+  const [editable, setEditable] = React.useState(false);
+
   const dob = new Date(selectedStudent.dob)
   const yyyy = dob.getFullYear();
   let mm = dob.getMonth() + 1; // Months start at 0!
@@ -49,6 +66,26 @@ function StudentInfo(props) {
     });
   }
 
+  const updateStudent = () => {
+    const updatedStudent = {
+      id: selectedStudent.id,
+      firstName,
+      lastName,
+      address,
+      dob: birth,
+      highSchool,
+      mathScore: selectedStudent.mathScore,
+      literatureScore: selectedStudent.literatureScore,
+      englishScore: selectedStudent.englishScore
+    }
+    StudentService.updateStudent(updatedStudent).then((res) => {
+      alert(res.data);
+      if(res.data === "Update successfully"){
+        dispatch(updateRefreshState(!stateRefresh));
+        setEditable(false);
+      }
+    });
+  }
   return(
     <Box sx={{
       display: 'flex',
@@ -93,7 +130,7 @@ function StudentInfo(props) {
               m: 2
             }}
             >
-              {selectedStudent.lastName} {selectedStudent.firstName}
+              {lastName} {firstName}
             </Typography>
           </Box>
         </Grid>
@@ -103,16 +140,37 @@ function StudentInfo(props) {
             <Typography sx = {{...fieldTypoStyle}}>Student ID</Typography>
           </Grid>
           <Grid item xs={6}>
-            <Typography sx = {{...valueTypoStyle}}>{selectedStudent.id}</Typography>
+            <Typography sx = {{...valueTypoStyle}}>
+              {selectedStudent.id}
+            </Typography>
           </Grid>
         </Grid>
 
         <Grid container item xs={12} sx={{...rowStyle}}>
           <Grid item xs={6}>
-            <Typography sx = {{...fieldTypoStyle}}>Name</Typography>
+            <Typography sx = {{...fieldTypoStyle}}>First Name</Typography>
           </Grid>
           <Grid item xs={6}>
-            <Typography sx = {{...valueTypoStyle}}>{selectedStudent.lastName} {selectedStudent.firstName}</Typography>
+            {editable ? <TextField sx = {{...valueTypoStyle}}
+                       value={firstName} onChange={(e) => {setFirstName(e.target.value)}}
+            >
+            </TextField> : <Typography sx = {{...valueTypoStyle}}>
+              {firstName}
+            </Typography>}
+          </Grid>
+        </Grid>
+
+        <Grid container item xs={12} sx={{...rowStyle}}>
+          <Grid item xs={6}>
+            <Typography sx = {{...fieldTypoStyle}}>Last Name</Typography>
+          </Grid>
+          <Grid item xs={6}>
+            {editable ? <TextField sx = {{...valueTypoStyle}}
+                                   value={lastName} onChange={(e) => {setLastName(e.target.value)}}
+            >
+            </TextField> : <Typography sx = {{...valueTypoStyle}}>
+              {lastName}
+            </Typography>}
           </Grid>
         </Grid>
 
@@ -121,7 +179,7 @@ function StudentInfo(props) {
             <Typography sx = {{...fieldTypoStyle}}>DOB</Typography>
           </Grid>
           <Grid item xs={6}>
-            <Typography sx = {{...valueTypoStyle}}>{formattedDob}</Typography>
+            {editable ? <TextField sx = {{...valueTypoStyle}} value={birth} onChange={(e) => {setDob(e.target.value)}}></TextField> : <Typography sx = {{...valueTypoStyle}}> {formattedDob}</Typography>}
           </Grid>    
         </Grid>
 
@@ -130,7 +188,7 @@ function StudentInfo(props) {
             <Typography sx = {{...fieldTypoStyle}}>Address</Typography>
           </Grid>
           <Grid item xs={6}>
-            <Typography sx = {{...valueTypoStyle}}>{selectedStudent.address}</Typography>
+            {editable ? <TextField sx = {{...valueTypoStyle}} value={address} onChange={(e) => {setAddress(e.target.value)}}></TextField> : <Typography sx = {{...valueTypoStyle}}> {address} </Typography>}
           </Grid> 
         </Grid>
 
@@ -139,7 +197,7 @@ function StudentInfo(props) {
             <Typography sx = {{...fieldTypoStyle}}>School</Typography>
           </Grid>
           <Grid item xs={6}>
-            <Typography sx = {{...valueTypoStyle}}>{selectedStudent.highSchool}</Typography>
+            {editable ? <TextField sx = {{...valueTypoStyle}} value={highSchool} onChange={(e) => {setHighSchool(e.target.value)}}></TextField> : <Typography sx = {{...valueTypoStyle}}> {highSchool} </Typography>}
           </Grid>  
         </Grid>  
         
@@ -149,12 +207,22 @@ function StudentInfo(props) {
             display: "flex",
             justifyContent: "center"
           }}>
-            <Button variant="contained" startIcon={<EditIcon />} sx={{mx:1}}>
-              <Typography sx={{...typoFont, fontSize: 12}}>Edit</Typography>
-            </Button>
-            <Button color="error" variant="contained" startIcon={<DeleteIcon />} sx={{mx:1, backgroud:"red", color:"white"}} onClick={() => {deleteStudent()}} >
-              <Typography sx={{...typoFont, fontSize: 12}}>Delete</Typography>
-            </Button>
+            {!editable ?
+              <><Button variant="contained" startIcon={<EditIcon/>} sx={{mx: 1}} onClick={() => {
+                setEditable(true);
+              }}>
+                <Typography sx={{...typoFont, fontSize: 12}}>Edit</Typography>
+              </Button><Button color="error" variant="contained" startIcon={<DeleteIcon/>}
+                               sx={{mx: 1, backgroud: "red", color: "white"}} onClick={() => {
+                deleteStudent();
+              }}>
+                <Typography sx={{...typoFont, fontSize: 12}}>Delete</Typography>
+              </Button></>
+              : <Button variant="contained" sx={{mx: 1}} startIcon={<DoneOutlineIcon/>} onClick={() => {
+                updateStudent();
+              }
+              }>Done</Button>
+            }
           </Box>
         </Grid>
       </Grid>
